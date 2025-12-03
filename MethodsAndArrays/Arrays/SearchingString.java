dd68f6ed7ff01d205d31e99ef0e94f8bcd0d8d28
NavneetSingh-98 <navneetrajput926@gmail.com>
2025-12-03 07:56:31 +0530

    Changes

--- MethodsAndArrays/Arrays/SearchingString.java
+++ MethodsAndArrays/Arrays/SearchingString.java
@@ -6,35 +6,63 @@ public class SearchingString {
     public static void main(String[] args) {
         //Searching String in Arrys 
 
-    Scanner input = new Scanner(System.in);
-    System.out.println("Welcome in the String Searching in Array ");
-    String[] myArr = {"Navneet","Shivam","Mohan","Rohan","Geeta","Rita","Ohm","Nana","Gudiya","Vishal"};
+    // Scanner input = new Scanner(System.in);
+    // System.out.println("Welcome in the String Searching in Array ");
+    // String[] myArr = {"Navneet","Shivam","Mohan","Rohan","Geeta","Rita","Ohm","Nana","Gudiya","Vishal"};
 
-    System.out.print("Enter Your Element You want to Search : ");
-    String newStrArr = input.next();
-    boolean isFound = isFound(myArr, newStrArr); 
-    if(isFound){
-        System.out.println("Your String is Found ");
-    }else{
-        System.out.println("Your String is Not Found ");
-    }
+    // System.out.print("Enter Your Element You want to Search : ");
+    // String newStrArr = input.next();
+    // boolean isFound = isFound(myArr, newStrArr); 
+    // if(isFound){
+    //     System.out.println("Your String is Found ");
+    // }else{
+    //     System.out.println("Your String is Not Found ");
+    // }
   
      // Methods  and Functions 
 
      
-    }
-    public static boolean isFound(String[] myArr,String newStrArr){
-        int index = 0;
-        while (index < newStrArr.length()) {
+    // }
+    // public static boolean isFound(String[] myArr,String newStrArr){
+    //     int index = 0;
+    //     while (index < newStrArr.length()) {
 
-            if(myArr[index] == newStrArr ){
+    //         if(myArr[index] == newStrArr ){
+    //             return true;
+    //         }
+    //         index++;
+            
+    //     }
+
+    //     return false;
+    // }
+
+    System.out.println("Welcome in the String Searching Program");
+    Scanner input = new Scanner(System.in);
+
+    String[] myArr  = {"Navneet", "Shivam","Aman","Sohan","Rohan"};
+
+    System.out.println("Enter Your String You want to Search : ");
+
+    String strArr = input.nextLine();
+
+    boolean isFound = isFound(myArr, strArr);
+
+    if(isFound){
+        System.out.println("Your String is Found");
+    }else{
+        System.out.println("Your String is not Found ");
+    }
+    }
+    public static boolean isFound(String[] myArr, String strArr){
+        int index = 0;
+        while(index < strArr.length()){
+            if(myArr[index] == strArr){
                 return true;
             }
             index++;
-            
         }
 
         return false;
     }
-
 }
